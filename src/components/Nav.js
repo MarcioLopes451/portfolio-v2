33d8155339e9bf808803d8c135820e7e5ac71f3a
Marcio Lopes <79576331+MarcioLopes451@users.noreply.github.com>
2023-05-22 13:40:40 +0100

    first commit

---
+++ src/components/Nav.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Github from '../images/icon-github.svg';
+import Linkedin from '../images/icon-linkedin.svg';
+import Twitter from '../images/icon-twitter.svg';
+
+
+export default function Nav() {
+  return (
+    <div className='navbar'>
+        <div className='navbar-container'>
+            <p>Marcio Lopes</p>
+            <div className='social-media'>
+                <img src={Github} alt='github' />
+                <img src={Linkedin} alt='linkedin' />
+                <img src={Twitter} alt='twitter' />
+            </div>
+        </div>
+    </div>
+  )
+}
