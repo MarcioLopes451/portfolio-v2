0e848b122ce5b3780944c65c58852a7bad2bc961
Marcio Lopes <79576331+MarcioLopes451@users.noreply.github.com>
2023-05-23 14:50:00 +0100

    project-section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Nav from './components/Nav';
 import Hero from './components/Hero';
+import Projects from './components/Projects';
 
 function App() {
   return (
     <div>
       <Nav />
       <Hero />
+      <Projects />
     </div>
   
   )
---
+++ src/components/Projects.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ThumbnailOne from '../images/thumbnail-project-1-large.webp';
+
+export const project = [
+    {
+        projectName: 'Reddit App',
+        lang: 'HTML CSS React',
+        image: ThumbnailOne,
+        website:'https://marciolopes451.github.io/React-Reddit-App/',
+        repo: 'https://github.com/MarcioLopes451/React-Reddit-App',
+    },
+    {
+        projectName: 'Reddit App',
+        lang: 'HTML CSS React',
+        image: ThumbnailOne,
+        website:'https://marciolopes451.github.io/React-Reddit-App/',
+        repo: 'https://github.com/MarcioLopes451/React-Reddit-App',
+    }
+]
+
+export default function Projects() {
+  return (
+    <div className='projects'>
+        <h1>Projects</h1>
+        <div className='projects-container'>
+        {project.map((item,index) => (
+            <div
+            key={index}>
+                <div className='content'>
+                    <div className='project-image'>
+                    <img src={item.image}/>
+                    <div className='project-links'>
+                        <a href={item.website} target="_blank">View Website</a>
+                        <a href={item.repo} target="_blank">View Source Code</a>
+                    </div>
+                    </div>
+                    <h2>{item.projectName}</h2>
+                    <p>{item.lang}</p>
+                </div>
+            </div>
+        ))}
+        </div>
+    </div>
+  )
+}
