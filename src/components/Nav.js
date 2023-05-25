fba53eb9c58c349c0f944f8405f7585f5bb9a95b
Marcio Lopes <79576331+MarcioLopes451@users.noreply.github.com>
2023-05-25 11:24:56 +0100

    mobile

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -15,6 +15,14 @@ export default function Nav() {
                 <img src={Twitter} alt='twitter' />
             </div>
         </div>
+        <div className='mobile-navbar'>
+        <p>Marcio Lopes</p>
+            <div className='social-mobile'>
+                <img src={Github} alt='github' />
+                <img src={Linkedin} alt='linkedin' />
+                <img src={Twitter} alt='twitter' />
+            </div>
+        </div>
     </div>
   )
 }
